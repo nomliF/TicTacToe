e591b1fa2e9097ab55faafa5e356e44e5738d804
Filmon <112778762+nomliF@users.noreply.github.com>
2023-03-27 11:22:38 +0200

    Initial Commit

---
+++ TicTacToe/TicTacToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeApp.swift
+//  TicTacToe
+//
+//  Created by Filmon Mehari Gebrezghi on 2023-03-27.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
